a2805b26868227216e7c323318f42f40324fca7d
Mohammadmehdi-web <webauctech110@gmail.com>
2025-03-17 16:04:21 +0530

    chnges database pase

--- db_con.php
+++ db_con.php
@@ -1,16 +1,14 @@
 <?php
     $servername = "localhost";
-    $username = "u622085619_auct_portfoUz";
-    $password = "Auctech@123";
-    $database = "u622085619_auct_portfoDb";
+    $username = "root";
+    $password = "";
+    $database = "spg_db";
 
     $con =  new mysqli($servername, $username, $password, $database);
 
     if($con->connect_error){
         die("Connection Failed: " . $con->connect_error);
     }
-    // Auctech@123
-    // u622085619_auct_portfoDb
-     // u622085619_auct_portfoUz
+    
 ?>
 
